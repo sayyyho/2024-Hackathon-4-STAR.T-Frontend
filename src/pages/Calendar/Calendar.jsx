584603e5ab82984644ba70c20ae6e27d9ed1543f
sayyyho <323psh@naver.com>
2024-07-31 11:58:46 +0000

    배포

--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -2,16 +2,10 @@ import * as S from "./styled";
 import { Header } from "../../components/common/Header/Header";
 import { CustomCalendar } from "../../components/CustomCalendar/CustomCalendar";
 import { getMonthCalenar } from "../../apis/calendar";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export const Calendar = () => {
-  const fetchData = async () => {
-    const res = await getMonthCalenar();
-    console.log(res);
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
+  useEffect(() => {}, []);
   return (
     <S.Layout>
       <Header $margin={"1rem 0 0 0"} $padding={"1rem 1rem 0 1rem"}>
